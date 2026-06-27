00ad06271fa4d9bcc04f97fcb692ed2830f50671
Shoshan Dagany <shoshan@gmail.com>
2026-06-27 18:40:38 +0300

    setup.ps1: update setup to work with powershell5

--- setup.ps1
+++ setup.ps1
@@ -1,5 +1,4 @@
 #requires -RunAsAdministrator
-#requires -Version 7.0
 
 if (-not ($Env:WT_SESSION)) {
     Throw "Windows Terminal (wt) is required."
@@ -7,11 +6,27 @@ if (-not ($Env:WT_SESSION)) {
 
 
 # Disable pwsh telemetry for funnzies :)
-[System.Environment]::SetEnvironmentVariable('POWERSHELL_TELEMETRY_OPTOUT','1','Machine')
+if ($PSVersionTable.PSVersion.Major -ge 7) {
+    [System.Environment]::SetEnvironmentVariable('POWERSHELL_TELEMETRY_OPTOUT', '1', 'Machine')
+} else {
+    $env:POWERSHELL_TELEMETRY_OPTOUT = "1"
+}
 
 Invoke-WebRequest -Uri https://github.com/JanDeDobbeleer/oh-my-posh/raw/main/themes/cobalt2.omp.json -OutFile $Home\cobalt2.omp.json
 
 attrib +h $Home\cobalt2.omp.json
 
-scoop install oh-my-posh zoxide JetBrainsMono-NF
+winget install DEVCOM.JetBrainsMonoNerdFont --source winget 
+$found =
+    (Test-Path "$env:WINDIR\Fonts\JetBrainsMonoNerdFont-Regular.ttf") -or
+    (Get-ChildItem "$env:WINDIR\Fonts" -ErrorAction SilentlyContinue | Where-Object Name -like "*JetBrains*")
+
+if ($found) {
+    Write-Host "JetBrains font is installed" -ForegroundColor Green
+} else {
+    Write-Host "JetBrains font is NOT installed" -ForegroundColor Red
+    exit 
+}
+
+scoop install oh-my-posh zoxide
 Write-Host "Successfully Installed CTT PowerShell Profile." -ForegroundColor Green
