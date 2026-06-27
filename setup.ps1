cd9bedefa26de7ca93d6dbeaace594b51e6b9272
itd22 <itd220222@gmail.com>
2026-06-27 12:16:00 +0300

    Refactor setup.ps1 for profile setup and installations
    Removed backup profile creation and updated installation commands.

--- setup.ps1
+++ setup.ps1
@@ -5,21 +5,14 @@ if (-not ($Env:WT_SESSION)) {
     Throw "Windows Terminal (wt) is required."
 }
 
-if (Test-Path $Profile) {
-    Move-Item -Path $Profile -Destination ($Profile + ".bak") -Force
-} else {
-    New-Item -Path $Profile -Force | Out-Null
-}
 
 # Disable pwsh telemetry for funnzies :)
 [System.Environment]::SetEnvironmentVariable('POWERSHELL_TELEMETRY_OPTOUT','1','Machine')
 
-Invoke-WebRequest -Uri https://github.com/ChrisTitusTech/powershell-profile/raw/main/Microsoft.PowerShell_profile.ps1 -OutFile $Profile
 Invoke-WebRequest -Uri https://github.com/JanDeDobbeleer/oh-my-posh/raw/main/themes/cobalt2.omp.json -OutFile $Home\cobalt2.omp.json
 
 attrib +h $Home\cobalt2.omp.json
 
-Install-Module -Name Terminal-Icons -Force -Repository PSGallery
 
 winget install JanDeDobbeleer.OhMyPosh ajeetdsouza.zoxide DEVCOM.JetBrainsMonoNerdFont --source winget --silent
 Write-Host "Successfully Installed CTT PowerShell Profile." -ForegroundColor Green
